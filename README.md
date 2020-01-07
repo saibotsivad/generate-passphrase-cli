4002d06655595def01def16e8603b3eea799d0ba
Tobias Davis <accounts@davistobias.com>
2020-01-07 13:45:35 -0600

    feature: add basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# generate-passphrase
-Generate a passphrase using random words, like battery-horse-staple.
+# generate-passphrase-cli
+
+Generate a passphrase from the CLI using random words, like `battery-horse-staple`.
+
+## install
+
+The usual way:
+
+```bash
+npm install generate-passphrase-cli
+```
+
+## options
+
+* `--help` Print version number and all available options.
+* `--min` Minimum number of characters in a word. Default is 3.
+* `--max` Maximum number of characters in a word. Default is 8.
+* `--words` Number of words in passphrase. Default is 5.
+* `--sep` Separator used between words. Default is `-`.
+
+## examples
+
+Use default settings:
+
+```bash
+$ generate-passphrase
+geminy-laughier-vest-muse-mantra-petard
+```
+
+Use lots of small words:
+
+```bash
+$ generate-passphrase --min=1 --max=3 --words=10
+zho-bod-arm-tis-oft-fun-yom-too-ays-es
+```
+
+Specify a custom separator:
+
+```bash
+$ generate-passphrase --sep="_" --words=2
+tuppence_colbys
+```
+
+Specify no separator:
+
+```bash
+$ generate-passphrase --sep="" --words=2
+tuppencecolbys
+```
+
+Use random numbers as separator (randomly selects an integer between 10 and 99):
+
+```bash
+$ generate-passphrase --sepnums --words=2
+tuppence23colbys
+```
+
+## license
+
+Published and released under the [Very Open License](http://veryopenlicense.com).
---
+++ index.js
@@ -0,0 +1,50 @@
+const { version } = require('./package.json')
+const randomWord = require('random-word')
+const sade = require('sade')
+
+const DEFAULT_MIN = 3
+const DEFAULT_MAX = 8
+const DEFAULT_WORDS = 6
+const DEFAULT_SEPARATOR = '-'
+
+sade('generate-passphrase', true)
+	.version(version)
+	.describe('Generate a passphrase using a list of random words.')
+	.example('# default options')
+	.example('--min=1 --max=3 # small words')
+	.example('--sep="" # no separator')
+	.option('--min', 'Minimum number of characters for a word. Default is 3.')
+	.option('--max', 'Maximum number of characters for a word. Default is 8.')
+	.option('--words', 'Number of words in passphrase. Default is 5.')
+	.option('--sep', 'Separator used between words. Default is "-".')
+	.option('--sepnums', 'Use a random number between words. Overrides separator option.')
+	.action(opts => {
+		opts.min = opts.min === undefined ? DEFAULT_MIN : parseInt(opts.min, 10)
+		opts.max = opts.max === undefined ? DEFAULT_MAX : parseInt(opts.max, 10)
+		opts.words = opts.words === undefined ? DEFAULT_WORDS : parseInt(opts.words, 10)
+
+		const getSeparator = isStart => {
+			if (isStart) {
+				return ''
+			} else if (opts.sepnums) {
+				return Math.floor(Math.random() * 99) + 10
+			} else if (opts.sep) {
+				return opts.sep
+			}
+			return DEFAULT_SEPARATOR
+		}
+
+		const words = new Array(opts.words)
+			.fill()
+			.map(() => {
+				let currentWord = randomWord()
+				while (currentWord.length < opts.min || currentWord.length > opts.max) {
+					currentWord = randomWord()
+				}
+				return currentWord
+			})
+			.reduce((string, word) => string + getSeparator(!string) + word, '')
+
+		process.stdout.write(words)
+	})
+	.parse(process.argv)
